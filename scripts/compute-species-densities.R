2a8ddf7dd2ff9b3807875aab74405ae376e4d6fa
jybarnagaud <jeanyves.barnagaud@gmail.com>
2026-04-10 11:12:22 +0200

    ridge plots for carabids and saproxylic species changed location of guildsp_colsxselec

--- scripts/compute-species-densities.R
+++ scripts/compute-species-densities.R
@@ -62,6 +62,8 @@ ridg2 <- ois4 %>%
 
 ridg2
 
+ggsave("outputs/birds-ridge-plot-all-species.png", width = 7, height = 15)
+
 # same ridge plot, restrict to the 15 most common species
 
 ois.b <- ois[,-c(1:7,47,48)]
@@ -79,6 +81,8 @@ ridg3 <- ois4.com10 %>%
   labs(x="Mixture (% oak vs pine + oak)",y="Species")
 
 ridg3
+
+ggsave("outputs/birds-ridge-plot-15-most-common-species.png", width = 7, height = 15)
 
 # same ridge plot, restrict to species with >= 10% regional occurrence
 
@@ -98,9 +102,9 @@ ridg4 <- ois4.frq10 %>%
 
 ridg4
 
-ggsave("outputs/ridge-plot-common-species.png", width = 7, height = 15)
+ggsave("outputs/birds-ridge-plot-common-species.png", width = 7, height = 15)
 
-## carabids
+## carabids---------------------------------------------------------------------
 
 # data
 
@@ -135,8 +139,74 @@ ridg.carab <- carab3 %>%
 
 ridg.carab
 
-## saproxylic stuff
+ggsave("outputs/carabid-ridge-plot-common-species.png", width = 7, height = 15)
+
+## saproxylic stuff-------------------------------------------------------------
 
 sapro <- read.csv2("data/tabplotsp_colsxselec.csv")
+keep.sp <- read.csv2("data/guildsp_colsxselec.csv")
+env.sapro <- read.csv2("data/Rel_Env_Saprox_2026.csv", dec = ".")
 
+# keep only some species defined by C. Bouget
+
+colkeep <- c("plot",keep.sp$species)
+sapro1 <- sapro[,colkeep]
+
+# reshape
+
+sapro.long <- pivot_longer(sapro1,cols=2:ncol(sapro1),names_to="species",values_to="count")
+
+# add mixture level
+
+env.sapro1 <- env.sapro[,c("plot","mel1.8ha")]
+sapro2 <- merge(sapro.long,env.sapro1,by="plot")
+sapro3 <- uncount(sapro2,weights=count)
+
+# add guilds to split the ridge plot
+
+sapro4 <- merge(sapro3,keep.sp,by = "species", all = F)
+
+decid.sapro4 <- subset(sapro4,guild == "decid")
+gene.sapro4 <- subset(sapro4,guild == "generalist")
+coni.sapro4 <- subset(sapro4,guild == "conif")
+
+# do the ridge plots per guild
+
+# do the ridge plot
+
+ridg.decid.sapro <- decid.sapro4 %>%
+  mutate(species = fct_reorder(species, mel1.8ha)) %>%
+  ggplot(aes(x = mel1.8ha, y = species)) + 
+  geom_density_ridges(scale=1, alpha=0.6,fill="black", colour="white")+
+  theme_classic()+
+  xlim(0,100)+
+  labs(x="Mixture (% oak vs pine + oak)",y="deciduous-related species")
+
+ridg.decid.sapro
+
+ggsave("outputs/saprox-deciduous-species-ridge-plot.png", width = 7, height = 15)
+
+ridg.coni.sapro <- coni.sapro4 %>%
+  mutate(species = fct_reorder(species, mel1.8ha)) %>%
+  ggplot(aes(x = mel1.8ha, y = species)) + 
+  geom_density_ridges(scale=1, alpha=0.6,fill="black", colour="white")+
+  theme_classic()+
+  xlim(0,100)+
+  labs(x="Mixture (% oak vs pine + oak)",y="conifer-related species")
+
+ridg.coni.sapro
+
+ggsave("outputs/saprox-conifer-species-ridge-plot.png", width = 7, height = 15)
+
+ridg.gene.sapro <- gene.sapro4 %>%
+  mutate(species = fct_reorder(species, mel1.8ha)) %>%
+  ggplot(aes(x = mel1.8ha, y = species)) + 
+  geom_density_ridges(scale=1, alpha=0.6,fill="black", colour="white")+
+  theme_classic()+
+  xlim(0,100)+
+  labs(x="Mixture (% oak vs pine + oak)",y="generalist species")
+
+ridg.gene.sapro
+
+ggsave("outputs/saprox-generalist-species-ridge-plot.png", width = 7, height = 15)
 
